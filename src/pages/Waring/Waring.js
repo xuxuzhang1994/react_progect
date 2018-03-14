16112afcfa31ebd347ae3cf9b173fac1111cc6a9
zhangjing <zhangjing@vchangy.com>
2018-03-14 14:27:23 +0800

    图表

--- src/pages/Waring/Waring.js
+++ src/pages/Waring/Waring.js
@@ -8,25 +8,16 @@ import {connect} from 'react-redux';
 class Waring extends Component {
     componentDidMount(){
         console.log('11111')
-        // this._https();
         this.chartDataOne();
-        // this.chartDataTwo();
+        this.chartDataTwo();
+        this.chartDataThree();
 
     }
-    // _https(){
-    //     api.getCharts().then((data)=>{
-    //         console.log('datas',data)
-    //     })
-    // }
     chartDataOne(){
          Highcharts.chart('container',{
              chart: {
                  polar: true,
                  type: 'line',
-                 style:{
-                     // backgroundColor:'#0075FC',
-                 },
-                 // plotBackgroundColor:'#0075FC',
              },
              title: {
                  text: '',
@@ -45,6 +36,9 @@ class Waring extends Component {
                  gridLineInterpolation: 'polygon',
                  lineWidth: 0,
                  min: 0
+             },
+             legend:{
+                 enabled: false
              },
              tooltip: {
                  shared: true,
@@ -70,20 +64,154 @@ class Waring extends Component {
     }
     chartDataTwo(){
         Highcharts.chart('container-two',{
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: ''
+            },
+            xAxis: {
+                categories: ['一月', '二月', '三月', '四月', '五月','六月','七月','八月','九月','十月','十月','十二月']
+            },
+            credits: {
+                enabled: false
+            },
+            legend:{
+                enabled: false
+            },
+            plotOptions:{
+                column: {
+                    pointPadding: 0,
+                    borderWidth: 0,
+                    groupPadding: 0,
+                    shadow: false
+                }
+            },
+            series: [{
+                name: '小张',
+                data: [5, 3, 4, 7, 2,1,3,4,4,3,2,4]
+            }]
 
+        })
+        Highcharts.chart('container-twos',{
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: ''
+            },
+            xAxis: {
+                categories: ['一月', '二月', '三月', '四月', '五月','六月','七月','八月','九月','十月','十月','十二月']
+            },
+            legend:{
+                enabled: false
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: '小张',
+                data: [5, 3, 4, 7, 2,1,3,4,4,3,2,4]
+            }]
 
         })
 
 
-    }
 
+    }
+    chartDataThree(){
+        var chart = null;
+        Highcharts.chart('container-three',{
+            chart: {
+                style: {
+                    // fontSize: '12px',
+                    fontWeight: 'bold',
+                    backgroundColor:null,
+                },
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                spacing : [10, 10 , 10, 10]
+            },
+            title: {
+                floating:true,
+                text: '数量65'
+            },
+            legend:{
+                enabled: false
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
+            },
+            colors:['#A1010D','#008325','#F39800'],
+            plotOptions: {
+                pie: {
+                    showInLegend:true,
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        },
+                        connectorWidth:0,
+                    },
+                    point: {
+                        events: {
+                            mouseOver: function(e) {  // 鼠标滑过时动态更新标题
+                                // 标题更新函数，API 地址：https://api.hcharts.cn/highcharts#Chart.setTitle
+                                chart.setTitle({
+                                    text: e.target.name+ '\t'+ e.target.y + ' %'
+                                });
+                            }
+                            //,
+                            // click: function(e) { // 同样的可以在点击事件里处理
+                            //     chart.setTitle({
+                            //         text: e.point.name+ '\t'+ e.point.y + ' %'
+                            //     });
+                            // }
+                        }
+                    },
+                }
+            },
+            series: [{
+                type: 'pie',
+                size: '80%',
+                innerSize: '60%',
+                name: '条',
+                data: [
+                    {
+                        name: 'Chrome',
+                        y: 20,
+                        sliced: true,
+                        selected: true,
+                        url: 'http://www.hcharts.cn'
+                    },
+                    ['Safari',    30],
+                    ['其他',   50]
+                ]
+            }]
+        }, function(c) {
+            // 环形图圆心
+            var centerY = c.series[0].center[1],
+                titleHeight = parseInt(c.title.styles.fontSize);
+            c.setTitle({
+                y:centerY + titleHeight/2
+            });
+            chart = c;
+        })
+    }
     render() {
         return (
             <div className="chart-item">
                 {/*<p>1111</p>*/}
-                <div className="chart-one" id="container"  ></div>
-                <div class="chart-two" id="container-two"></div>
-                <div class="chart-three" id="container-three"></div>
+                <div className="charts-one"><div className="chart-one" id="container"  ></div></div>
+                <div className="charts-two">
+                    <div className="chart-two" id="container-two"></div>
+                    <div className="chart-twos" id="container-twos"></div>
+                </div>
+                <div className="charts-three"><div className="chart-three" id="container-three"></div></div>
             </div>
         )
     }
